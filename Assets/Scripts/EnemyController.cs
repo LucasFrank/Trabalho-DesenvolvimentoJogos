a831653634f05cc0b22c749de5f75de4cc92e8d8
Lucas <lucas.r.frank@gmail.com>
2018-11-19 23:29:21 -0200

    Player movimentando e atacando, bug no pulo

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D coll) {
+        if (coll.gameObject.tag == "AttackingCollider") {
+            Debug.Log("uhhh");
+            Destroy(gameObject);
+        }
+
+        if (coll.gameObject.tag == "ComboCollider") {
+            Debug.Log("aaaaaahhh");
+            Destroy(gameObject);
+        }
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,16 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState {
+    Standing,
+    Running,
+    Jumping,
+    Attacking,
+    Combo,
+    StandDown,
+    StandUp
+};
+
 public class PlayerController : MonoBehaviour {
 
-    private Animator animationPlayer;
-    private float walkingSpeed;
+    private Animator animatorPlayer;
+    private Collider2D attackingCollider;
+    private Collider2D comboCollider;
 
-	// Use this for initialization
-	void Start () {
-        walkingSpeed = 0.02f;
-        animationPlayer = GetComponent<Animator>();
-        animationPlayer.SetBool("Walking", false);
+    private PlayerState state;
+    private bool grounded = false;
+    private float movingSpeed = 5;
+    private float jumpForce = 5;
+
+    // Use this for initialization
+    void Start () {
+        this.state = PlayerState.Standing;
+        this.animatorPlayer = GetComponent<Animator>();
+        this.attackingCollider = GameObject.FindGameObjectWithTag("AttackingCollider").GetComponent<Collider2D>();
+        this.comboCollider = GameObject.FindGameObjectWithTag("ComboCollider").GetComponent<Collider2D>();
     }
 	
 	// Update is called once per frame
@@ -20,33 +37,86 @@ public class PlayerController : MonoBehaviour {
 	}
 
     void inputHandler() {
-        if (Input.GetKey(KeyCode.RightArrow)) {
-            if (animationPlayer.GetBool("Walking") == false) {
-                animationPlayer.SetBool("Walking", true);
-            }
-            
-            transform.localScale = new Vector3(1, 1, 1);
-            transform.Translate(new Vector3(walkingSpeed, 0, 0));
-            //transform.position += new Vector3(walkingSpeed, 0, 0);
+        float dx = Input.GetAxis("Horizontal");
+        float dy = Input.GetAxis("Vertical");
 
-        } else if (Input.GetKey(KeyCode.LeftArrow)) {
-            if (animationPlayer.GetBool("Walking") == false) {
-                animationPlayer.SetBool("Walking", true);
-            }
-            transform.localScale = new Vector3(-1, 1, 1);
-            transform.Translate(new Vector3(-walkingSpeed, 0, 0));
-            //transform.position += new Vector3(walkingSpeed, 0, 0);
+        GetComponent<Rigidbody2D>().velocity = new Vector3(movingSpeed * dx, GetComponent<Rigidbody2D>().velocity.y, 0);
 
-        } else {
-            animationPlayer.SetBool("Walking", false);
+        if (this.state != PlayerState.StandDown) {
+            if (dx != 0) {
+
+                if (dx > 0)
+                    transform.localScale = new Vector3(-1, 1, 1); //Flipping
+                else
+                    transform.localScale = new Vector3(1, 1, 1); //Flipping
+
+                if (this.state != PlayerState.Jumping)
+                    this.state = PlayerState.Running;
+            } else {
+                if (this.state != PlayerState.Jumping)
+                    this.state = PlayerState.Standing;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                if (this.grounded) {
+                    GetComponent<Rigidbody2D>().velocity = new Vector3(GetComponent<Rigidbody2D>().velocity.x, jumpForce, 0);
+                    this.grounded = false;
+                    this.state = PlayerState.Jumping;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.X)) {
+                this.state = PlayerState.Attacking;
+            }
+
+            if (Input.GetKeyDown(KeyCode.C)) {
+                this.state = PlayerState.Combo;
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            //rd.velocity = new Vector3(0, jumpForce, 0);
-            //rd.AddForce(new Vector3(0, jumpForce, 0));
-            //animation.SetBool("Jumping", true);
-
-
+        if(dy != 0) {
+            if (dy > 0) {
+                if(this.state == PlayerState.StandDown)
+                    this.state = PlayerState.StandUp;
+            } else {
+                if(this.state == PlayerState.Standing)
+                    this.state = PlayerState.StandDown;
+            }
         }
+
+        this.animatorPlayer.SetBool("Running", this.state == PlayerState.Running);
+        this.animatorPlayer.SetBool("Jumping", this.state == PlayerState.Jumping);
+        this.animatorPlayer.SetBool("StandUp", this.state == PlayerState.StandUp);
+        this.animatorPlayer.SetBool("StandDown", this.state == PlayerState.StandDown);
+        this.animatorPlayer.SetBool("Attacking", this.state == PlayerState.Attacking);
+        this.animatorPlayer.SetBool("Combo", this.state == PlayerState.Combo);
     }
+
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        this.grounded = true;
+        this.state = PlayerState.Standing;
+    }
+
+    public void activateAttacking() {
+        this.attackingCollider.enabled = true;
+        this.attackingCollider.isTrigger = true;
+    }
+
+    public void activateCombo() {
+        this.comboCollider.enabled = true;
+        this.comboCollider.isTrigger = true;
+    }
+
+    public void deactivateAttacking() {
+        this.attackingCollider.enabled = false;
+        this.attackingCollider.isTrigger = false;
+    }
+
+    public void deactivateCombo() {
+        this.comboCollider.enabled = false;
+        this.comboCollider.isTrigger = false;
+    }
+
+
 }
