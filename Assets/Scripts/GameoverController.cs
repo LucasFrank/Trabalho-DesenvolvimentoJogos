8cf3e9b0a37f54b4b1d885ead1b694f3b2275897
Lucas <lucas.r.frank@gmail.com>
2018-11-21 00:32:49 -0200

    Tela de Menu, GameOver, com alguns botões funcionando

---
+++ Assets/Scripts/GameoverController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameoverController : MonoBehaviour {
+
+    public void goToMenu() {
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+    public void startGame() {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void loadGame() {
+        //Load last game
+    }
+
+    public void optionsUI() {
+
+    }
+
+    public void quitGame() {
+        Application.Quit();
+    }
+}
